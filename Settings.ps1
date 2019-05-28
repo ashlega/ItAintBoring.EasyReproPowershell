1caf12b7d6dcfc7ab8e17a0ce47771652d544765
Alex Shlega <Alex.Shlega@ottawa.ca>
2019-05-28 07:51:48 -0400

    Settings changes

--- Settings.ps1
+++ Settings.ps1
@@ -1,3 +1,8 @@
-$Env:XrmUserName = "info@treecatsoftware.com"
-$Env:XrmUrl = "https://treecatsoftware.crm3.dynamics.com"
-$Env:XrmPassword = $null
+if($Env:XrmUserName -eq $null)
+{
+   $Env:XrmUserName = "info@treecatsoftware.com"
+}
+if($Env:XrmUrl -eq $null)
+{
+   $Env:XrmUrl = "https://treecatsoftware.crm3.dynamics.com"
+}
